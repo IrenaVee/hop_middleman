41819257fb42183ad5ca26ca1b184e9104723777
IrenaVee <irena.vieira@gmail.com>
2016-08-31 16:44:54 +0200

    added jquery code to all.js. Saving file to push commits to Github and continue later

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
-// This is where it all goes :)
+$(document).ready(function() {
+
+	var $toggleButton = $('.toggle-button'),
+    	$menuWrap = $('.menu-wrap'),
+			$menuButton = $('.menu');
+
+	// Hamburger button
+
+	$toggleButton.on('click', function() {
+		$(this).toggleClass('button-open'); //refereert aan .button-open in CSS
+		$menuWrap.toggleClass('menu-show'); //refereert aan .menu-show in CSS
+		$menuButton.toggleClass('menu-open'); //refereert aan .menu-open in CSS
+		console.log($(this));
+	});
+
+});
